9e9b8b8c2ce41551e38ca3dec4f97b97663bdc1a
helderrodrigues <informatica@ryazbek.com.br>
2023-06-16 18:03:27 -0300

    Tudo em ordem

---
+++ scripts/InstallTeamViewer.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Unrestricted -Force
+Write-Progress -Activity 'Getting TeamViewer 12' -PercentComplete (100/10 * 1)
+New-Item -Type Directory -Path "C:\Temp\Core\Tools\Remote\TeamViewer\" -Force
+Invoke-WebRequest -Uri "https://download.teamviewer.com/download/version_12x/TeamViewer_Setup.exe" -OutFile 'C:\Temp\Core\Tools\Remote\TeamViewer\TeamViewer_Setup.exe'
+Start-Process "C:\Temp\Core\Tools\Remote\TeamViewer\TeamViewer_Setup.exe"
