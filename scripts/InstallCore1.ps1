a73fae91f0a16fcd14e9a365f0dc99c116dc695b
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-06-19 16:30:00 -0300

    Update InstallCore1.ps1

--- scripts/InstallCore1.ps1
+++ scripts/InstallCore1.ps1
@@ -34,19 +34,13 @@ exit
 Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq "Microsoft Update Health Tools"} | foreach-object -process {$_.Uninstall()}
 
 #Remove OneDrive
-Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq "Microsoft OneDrive"} | foreach-object -process {$_.Uninstall()}
+#Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq "Microsoft OneDrive"} | foreach-object -process {$_.Uninstall()}
 
 Write-Progress -Activity 'Now configuring your Power Plan' -PercentComplete (100/10 * 1)
 powershell.exe -File C:\Temp\Core\Scripts\PowerCFG.ps1
 
-Write-Progress -Activity 'Installing ATERA' -PercentComplete (100/10 * 2)
-powershell.exe -File C:\Temp\Core\Scripts\InstallATERA.ps1
-
 Write-Progress -Activity 'Installing AV ESET' -PercentComplete (100/10 * 3)
 powershell.exe -File C:\Temp\Core\Scripts\InstallAVESET.ps1
-
-Write-Progress -Activity 'Installing Forticlient VPN' -PercentComplete (100/10 * 9)
-powershell.exe -File C:\Temp\Core\Scripts\InstallVPN.ps1
 
 Write-Progress -Activity 'Installing AnyDesk' -PercentComplete (100/10 * 2)
 powershell.exe -File C:\Temp\Core\Scripts\InstallAnyDesk.ps1
@@ -86,7 +80,6 @@ powershell.exe -File C:\Temp\Core\Scripts\InstallChocolatey.ps1
 
 Write-Progress -Activity 'Installing Microsoft 365' -PercentComplete (100/10 * 8)
 powershell.exe -File C:\Temp\Core\Scripts\InstallM365.ps1
-appwiz.cpl
 
 Stop-Transcript
 
