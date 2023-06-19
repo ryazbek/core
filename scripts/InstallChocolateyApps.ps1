ded2e433b6f7f683218e93830576baab6a44cbbb
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-06-19 14:18:26 -0300

    Update version Teams InstallChocolateyApps.ps1

--- scripts/InstallChocolateyApps.ps1
+++ scripts/InstallChocolateyApps.ps1
@@ -7,7 +7,7 @@ choco install googlechrome -y --force
 #choco install microsoft-teams -y --force
 Write-Progress -Activity 'Getting Microsoft Teams' -PercentComplete (100/10 * 1)
 New-Item -Type Directory -Path "C:\Temp\Core\Programs\Microsoft Office\" -Force
-[system.Diagnostics.Process]::Start("chrome.exe","https://statics.teams.cdn.office.net/production-windows/1.5.00.11163/TeamsSetup.exe")
+[system.Diagnostics.Process]::Start("chrome.exe","https://statics.teams.cdn.office.net/production-windows/1.6.00.12455/TeamsSetup.exe")
 Copy-Item "$env:userprofile\Downloads\TeamsSetup.exe" "C:\Temp\Core\Programs\Microsoft Office\"
 choco install notepadplusplus -y --force
 choco install speedtest -y --force
