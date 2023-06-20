c0d52acb850d359743fc4497961cb1a1d36abe4a
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-06-20 17:26:54 -0300

    Update and rename InstallCore1.ps1 to InstallCore.ps1

--- scripts/InstallCore.ps1
+++ scripts/InstallCore.ps1
@@ -99,6 +99,9 @@ powershell.exe -File C:\Temp\Core\Scripts\InstallSupportAssistIntel.ps1
 Write-Progress -Activity 'Installing Microsoft 365' -PercentComplete (100/10 * 8)
 powershell.exe -File C:\Temp\Core\Scripts\InstallMSTeams.ps1
 
+Write-Progress -Activity 'Getting Installed Programs' -PercentComplete (100/10 * 8)
+powershell.exe -File C:\Temp\Core\Scripts\InstalledPrograms.ps1
+
 explorer shell:AppsFolder
 appwiz.cpl
 devmgmt.msc
@@ -111,46 +114,11 @@ explorer ms-windows-store:
 
 Stop-Transcript
 
-function Analyze( $p, $f) {
-
-  Get-ItemProperty $p |ForEach-Object {
-  
-  if (($_.DisplayName) -or ($_.version)) {
-  
-  [PSCustomObject]@{
-  
-  From = $f;
-  
-  Name = $_.DisplayName;
-  
-  Version = $_.DisplayVersion;
-  
-  Install = $_.InstallDate
-  
-  }
-  
-  }
-  
-  }
-  
-  }
-  
-  $s = @()
-  
-  $s += Analyze 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' 64
-  
-  $s += Analyze 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' 32
-  
-  $s | Sort-Object -Property Name |Export-Csv C:\Temp\InstalledPrograms_$(Get-Content env:computername).csv
-
-
-Get-Content "C:\Temp\Core\InstallCoreErrors1.txt" | Out-GridView -PassThru -Title "LOG"
-
 dism /online /enable-feature /featurename:netfx3 /all
 dism /online /enable-feature /featurename:WCF-HTTP-Activation /all
 dism /online /enable-feature /featurename:WCF-NonHTTP-Activation /all
 
-appwiz.cpl
+Get-Content "C:\Temp\Core\InstallCoreErrors1.txt" | Out-GridView -PassThru -Title "LOG"
 
 Write-Progress -Activity 'Checking Windows Update' -PercentComplete (100/10 * 10)
 powershell.exe -File C:\Temp\Core\Scripts\CheckAndInstallWU.ps1
