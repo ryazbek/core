4600471b6abb98b6f985c63ad91f2072f5e653a7
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-06-23 13:50:55 -0300

    Update InstallIntelRST.ps1

--- scripts/InstallIntelRST.ps1
+++ scripts/InstallIntelRST.ps1
@@ -4,3 +4,4 @@ New-Item -Type Directory -Path "C:\Temp\Core\Drivers\Intel\" -Force
 #Copy-Item "D:\SetupRST.exe" "C:\Temp\Core\Drivers\Intel\"
 Invoke-WebRequest -Uri "https://downloadmirror.intel.com/751275/SetupRST.exe" -OutFile "C:\Temp\Core\Drivers\Intel\SetupRST.exe"
 & "C:\Temp\Core\Drivers\Intel\SetupRST.exe" -silent -accepteula
+# https://www.intel.com.br/content/www/br/pt/support/articles/000057787/memory-and-storage/intel-optane-memory.html
