cdc7bedced5679e9d7ebc3b414371d5ddbad091d
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-06-23 14:52:57 -0300

    Update InstallMSTeams.ps1

--- scripts/InstallMSTeams.ps1
+++ scripts/InstallMSTeams.ps1
@@ -5,7 +5,7 @@ Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq "Microsoft Teams"
 Write-Progress -Activity 'Getting Microsoft Teams' -PercentComplete (100/10 * 1)
 New-Item -Type Directory -Path "C:\Temp\Core\Programs\Microsoft Office\" -Force
 [system.Diagnostics.Process]::Start("chrome.exe","https://statics.teams.cdn.office.net/production-windows/1.6.00.12455/TeamsSetup.exe")
-Start-Sleep -Seconds 15
+Sleep 15
 Move-Item "$env:userprofile\Downloads\TeamsSetup.exe" "C:\Temp\Core\Programs\Microsoft Office\"
 & "C:\Temp\Core\Programs\Microsoft Office\TeamsSetup.exe"
 #https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true
