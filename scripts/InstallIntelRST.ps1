e72628b3ff3f26cd4b009c2e085fe8befab6d021
ryazbek <136825526+ryazbek@users.noreply.github.com>
2023-06-23 13:47:45 -0300

    Create InstallIntelRST.ps1

---
+++ scripts/InstallIntelRST.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy Unrestricted -Force
+Write-Progress -Activity 'Installing Intel RST' -PercentComplete (100/10 * 1)
+New-Item -Type Directory -Path "C:\Temp\Core\Drivers\Intel\" -Force
+#Copy-Item "D:\SetupRST.exe" "C:\Temp\Core\Drivers\Intel\"
+Invoke-WebRequest -Uri "https://downloadmirror.intel.com/751275/SetupRST.exe" -OutFile "C:\Temp\Core\Drivers\Intel\SetupRST.exe"
+& "C:\Temp\Core\Drivers\Intel\SetupRST.exe" -silent -accepteula
