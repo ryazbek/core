b37d71faa6a10460f467d93de33b42ec74739e5d
ryazbek <136825526+ryazbek@users.noreply.github.com>
2025-02-12 08:38:58 -0300

    Update InstallChocolatey.ps1

--- scripts/InstallChocolatey.ps1
+++ scripts/InstallChocolatey.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy RemoteSigned -Scope Process
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 refreshenv
